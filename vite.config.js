2571804383d5317902b5e7e90aa2bedc99710ed0
iphint <ma7804058@gmail.com>
2026-05-25 21:28:31 +0700

    proxy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   server: {
-    port: 5173
-  }
+    proxy: {
+      "/api": {
+        target: "http://202.10.44.139:5001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
